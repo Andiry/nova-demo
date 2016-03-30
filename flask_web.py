26b28b2a7e6a2b1d5c2d4135c1a80f0343db4596
Andiry Xu <andiry.xu@gmail.com>
2016-03-30 16:13:07 +0800

    Scripts test

---
+++ flask_web.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template,request
+import time
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+import json
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/data')
+def data():
+	arr = []
+	arr.append([int(time.time()), 100])
+	return json.dumps(arr)
+
+if __name__ == '__main__':
+	app.run(host='127.0.0.1', port=9092, debug=True)
+
+
