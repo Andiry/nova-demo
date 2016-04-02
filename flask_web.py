f0fd96b4666aff7df357f74155450258e4a1d34c
Amirsaman Memaripour <amemarip@eng.ucsd.edu>
2016-04-01 22:33:23 -0700

    adding support for two bars

--- flask_web.py
+++ flask_web.py
@@ -5,6 +5,7 @@ import getops
 import sys, getopt
 import getargs
 import json
+import random
 
 app = Flask(__name__)
 
@@ -18,11 +19,16 @@ def index():
 @app.route('/data')
 def data():
 	arr = []
+	arr.append([])
+	arr.append([])
 	ops = getops.getkey(conn)
-	arr.append([int(time.time()) * 1000, float(ops)])
+	arr[0].append([int(time.time()) * 1000, random.uniform(1, 1000)])
+	#arr[0].append([int(time.time()) * 1000, float(ops)])
+	arr[1].append([int(time.time()) * 1000, random.uniform(1, 1000)])
+	#arr[1].append([int(time.time()) * 1000, float(ops) * 10])
 	return json.dumps(arr)
 
 if __name__ == '__main__':
-	app.run(host='localhost', port=9092, debug=True)
+	app.run(host='132.239.95.31', port=80, debug=False)
 
 
